9114a41f310c86b817ef17054f8092539b9df420
antoineefrei <55093994+antoineefrei@users.noreply.github.com>
2021-12-03 10:49:23 +0100

    Add files via upload

---
+++ MIKOLAJCZAK_Antoine_TP1_ST2TRD/MIKOLAJCZAK_Antoine_TP1_ST2TRD/Exercice3.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MIKOLAJCZAK_Antoine_TP1_ST2TRD
+{
+    public class Exercice3
+    {
+        private static int PowerFunction(int x)
+        {
+            return (int)(Math.Pow(x, 2) -4);
+        }
+
+        public void GestionException()
+        {
+            var eq = 0;
+           
+            for (int i = -3; i <= 3; i++) {
+                try
+                {
+                    eq = 10 / PowerFunction(i);
+                    Console.WriteLine("f(" + i + ") = " + eq);
+                }
+                catch (DivideByZeroException)
+                { 
+                    Console.WriteLine("Division by zero.");
+                }
+
+                    
+            }
+        }
+    }
+}
